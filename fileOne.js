f8e27c7ef06bfb7a354db0e7c355ca0ad56c3e18
Deepak <deepaknnayak34@gmail.com>
2024-01-22 19:34:08 +0530

    updated

---
+++ fileOne.js
@@ -0,0 +1,21 @@
+// function getData(){
+//     var name = "deepak"
+    
+//     function getName(){
+//         console.log(name)
+//     }
+//     getName()
+// }
+// console.log(name)
+// getData()
+// function getData(){
+//     function getName(){
+//         let name = "deepak"
+//         const title = "kumar"
+//     }
+    
+//     console.log(title)
+//     getName()
+// }
+
+// getData()
