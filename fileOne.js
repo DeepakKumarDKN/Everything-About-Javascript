16eac1b903674a8ca282d67e813196b33e2d0d61
Deepak <deepaknnayak34@gmail.com>
2024-01-30 11:39:28 +0530

    Updated

--- fileOne.js
+++ fileOne.js
@@ -1,10 +1,7 @@
-const users = [ 
-    {userName: "deepak_kumar", firstName: "deepak", middleName: "kumar"},
-    {userName: "rahul_krishna", firstName: "rahul", middleName: "krishna"},
-    {userName: "aly_goni", firstName: "aly", middleName: "singh"}
-]
+getName()
+console.log(x)
 
-const [{userName}] = users
-users.map((i)=>{
-    console.log(i.userName)
-})
+var x = 7;
+function getName(){
+    console.log('namestey javascript')
+}
